a6b27355e300d3ab5d5c2071d5ab49d9153aa977
nazmultamim <nazmultamim921@gmail.com>
2026-07-25 12:31:44 +0600

    verify

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,9 @@ export const metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  verification: {
+    google: "1nkLqb6rVmU_3u7tbSLi9d5-ojdlCXiK84P8CoYADYc",
+  },
   alternates: {
     canonical: '/',
   },
