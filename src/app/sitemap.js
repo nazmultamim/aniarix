ac0ea6b4bc19a32a24e4531a6530b91cbd4c5e27
nazmultamim <nazmultamim921@gmail.com>
2026-07-25 12:12:48 +0600

    v0.2.1

---
+++ src/app/sitemap.js
@@ -0,0 +1,38 @@
+const BASE_URL = "https://www.aniarix.site";
+
+export default function sitemap() {
+  const lastModified = new Date();
+
+  return [
+    {
+      url: BASE_URL,
+      lastModified,
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/home`,
+      lastModified,
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${BASE_URL}/anime`,
+      lastModified,
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${BASE_URL}/trending`,
+      lastModified,
+      changeFrequency: "hourly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
